ad06384a154a68d4be0ed85ead48c7363588fc8d
HarriiM04 <143804990+HarriiM04@users.noreply.github.com>
2025-05-23 19:07:53 +0530

    Create index.php

---
+++ Project/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jenkins HandsOn</title>
+</head>
+<body style="background-image: url('images/1.jpg');">
+   <h1>Welcome !!!!!!!!!!!</h1>
+</body>
+</html>

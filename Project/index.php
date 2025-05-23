038dd7aedb7996d1249158d933f13dbec2247ccf
HarriiM04 <143804990+HarriiM04@users.noreply.github.com>
2025-05-23 20:00:52 +0530

    Update index.php

--- Project/index.php
+++ Project/index.php
@@ -4,6 +4,6 @@
     <title>Jenkins HandsOn</title>
 </head>
 <body style="background-image: url('Images/github.png');">
-   <h1>Welcome !!!!!!!!!!!</h1>
+   <h1 style="color: white;">Welcome !!!!!!!!!!!</h1>
 </body>
 </html>
